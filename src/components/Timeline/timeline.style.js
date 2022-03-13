10c8fed5e029257c17b0f29d7d669569c05e5c0f
Celdama <celdama.dev@gmail.com>
2022-03-13 19:51:30 +0100

    components/Timeline/timeline.style: update

--- src/components/Timeline/timeline.style.js
+++ src/components/Timeline/timeline.style.js
@@ -3,8 +3,6 @@ import styled from 'styled-components';
 export const Wrapper = styled.section`
   width: 80%;
   outline: 1px solid red;
-  /* display: flex;
-  justify-content: center; */
 `;
 
 export const Content = styled.div``;
