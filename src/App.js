5ff948599c97bc64820316fcada7fb62ea531b47
Celdama <celdama.dev@gmail.com>
2022-03-13 14:08:49 +0100

    src: create navbar style files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { GlobalStyle } from './GlobalStyle';
+import { NavBar } from './layouts/NavBar';
 
 export const App = () => {
   return (
     <div className='App'>
+      <NavBar />
       <h1>Celdagram</h1>
       <GlobalStyle />
     </div>
