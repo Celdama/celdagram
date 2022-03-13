f78015a005b9698cf1064a56ff16e3acadd2b43f
Celdama <celdama.dev@gmail.com>
2022-03-13 19:09:24 +0100

    components/Home: create content and style to Home component

--- src/components/Home/home.style.js
+++ src/components/Home/home.style.js
@@ -7,9 +7,45 @@ export const Content = styled.div`
 `;
 
 export const ContentPhotos = styled.div`
-  width: 70%;
+  width: 80%;
 `;
 
 export const ContentUser = styled.div`
-  width: 30%;
+  width: 20%;
+
+  .content-user-info {
+    outline: 1px solid red;
+    justify-content: center;
+    display: flex;
+    gap: 1rem;
+    align-items: center;
+
+    img {
+      width: 50px;
+      height: 50px;
+      border-radius: 25px;
+    }
+  }
+
+  .content-user-suggestion {
+    outline: 1px solid red;
+
+    .suggestion-user {
+      outline: 1px solid blue;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      div {
+        display: flex;
+        gap: 1rem;
+      }
+
+      img {
+        margin: 5px;
+        width: 40px;
+        border-radius: 20px;
+      }
+    }
+  }
 `;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,7 +6,29 @@ export const Home = () => {
     <Wrapper>
       <Content>
         <ContentPhotos>photos content</ContentPhotos>
-        <ContentUser>user content</ContentUser>
+        <ContentUser>
+          <div className='content-user-info'>
+            <img
+              src='https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'
+              alt='user avatar'
+            />
+            <h4>John Doe</h4>
+          </div>
+          <div className='content-user-suggestion'>
+            <h5>Suggestions</h5>
+
+            <div className='suggestion-user'>
+              <div>
+                <img
+                  src='https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'
+                  alt=''
+                />
+                <p>Foo Foo</p>
+              </div>
+              <button>follow</button>
+            </div>
+          </div>
+        </ContentUser>
       </Content>
     </Wrapper>
   );
