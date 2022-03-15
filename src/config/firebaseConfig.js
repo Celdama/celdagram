5db22a9c182de56346f243aecd2d5f1bdd0cd03f
Celdama <celdama.dev@gmail.com>
2022-03-15 17:17:06 +0100

    add firebase config files

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB83rScDOC19T_BLxizjQYFm24TzE1Dl6s',
+  authDomain: 'celdagram.firebaseapp.com',
+  projectId: 'celdagram',
+  storageBucket: 'celdagram.appspot.com',
+  messagingSenderId: '509039589581',
+  appId: '1:509039589581:web:88be0cd0f79608fa359184',
+  measurementId: 'G-K7GX0M6BW6',
+};
+
+const app = initializeApp(firebaseConfig);
