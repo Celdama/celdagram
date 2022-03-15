032aa31a8930d901d464af4d8f5f3289c67db60d
Celdama <celdama.dev@gmail.com>
2022-03-15 08:11:12 +0100

    src: add BrowserRouter to handle profil route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { GlobalStyle } from './GlobalStyle';
 import { NavBar } from './layouts/NavBar';
 import { Main } from './layouts/Main';
 import { Home } from './components/Home';
+import { Profile } from './components/Profile';
 
 export const App = () => {
   return (
     <div className='App'>
-      <NavBar />
-      <Main>
-        <Home />
-      </Main>
-      <GlobalStyle />
+      <BrowserRouter>
+        <NavBar />
+        <Main>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route path='/profile' element={<Profile />} />
+          </Routes>
+          {/* <Home /> */}
+        </Main>
+        <GlobalStyle />
+      </BrowserRouter>
     </div>
   );
 };
