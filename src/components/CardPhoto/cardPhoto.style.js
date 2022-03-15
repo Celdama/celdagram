d22291a85d881c85e64b5f1ab9594df99d2c96bb
Celdama <celdama.dev@gmail.com>
2022-03-15 07:44:56 +0100

    components/CardPhoto: add style

--- src/components/CardPhoto/cardPhoto.style.js
+++ src/components/CardPhoto/cardPhoto.style.js
@@ -1,11 +1,81 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.article`
-  height: 800px;
+  /* height: 800px; */
   width: 504px;
   margin-bottom: 30px;
-  outline: 1px solid lightgrey;
+  outline: 1px solid lightgray;
   background-color: #fff;
+  border-radius: 4px;
+
+  .icon {
+    height: 1.25rem;
+    width: 1.25rem;
+  }
+
+  .icons-wrapper,
+  .likes-wrapper {
+    padding: 0;
+    font-size: 0.9rem;
+  }
+
+  .content-post {
+    padding: 0.3rem 0.6rem;
+  }
+
+  .likes-wrapper {
+    margin: 0.4rem 0;
+    font-weight: bold;
+  }
+
+  .like {
+    color: red;
+  }
+
+  .date-wrapper {
+    padding: 0.4rem 0;
+    font-size: 0.68rem;
+    text-transform: uppercase;
+    font-weight: bold;
+    color: #6c757d;
+  }
+
+  .add-comment-wrapper {
+    margin: 0;
+    height: 36px;
+    padding: 0;
+    border-top: 1px solid lightgray;
+    display: flex;
+
+    input {
+      width: 90%;
+      padding: 0 0.6rem;
+      border: none;
+    }
+
+    button {
+      border: none;
+      font-size: 0.7rem;
+      background-color: transparent;
+      width: 10%;
+      color: #6c757d;
+
+      :hover {
+        cursor: pointer;
+      }
+    }
+  }
+
+  .comments-wrapper {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+    font-size: 0.9rem;
+
+    .author {
+      font-weight: bold;
+    }
+  }
 
   div {
     padding-left: 0.6rem;
--- src/components/CardPhoto/index.js
+++ src/components/CardPhoto/index.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import { UserAvatar } from '../UserAvatar';
 import { Wrapper } from './cardPhoto.style';
+import { HeartIcon, ChatIcon } from '@heroicons/react/outline';
+import { HeartIcon as HeartIconSolid } from '@heroicons/react/solid';
+
+const comments = [
+  {
+    author: 'johndoe',
+    comments: 'My desktop',
+  },
+  {
+    author: 'marshal',
+    comments: 'love this place !',
+  },
+  {
+    author: 'orwell',
+    comments: 'simplicity',
+  },
+];
 
 export const CardPhoto = ({ url }) => {
   return (
@@ -13,13 +30,32 @@ export const CardPhoto = ({ url }) => {
         />
       </div>
       <img className='photo' src={url} alt='desc' />
-      <div>
-        <p>like comments</p>
-        <p>2 likes</p>
-        <p>list comments</p>
-        <p>list comments</p>
-        <p>list comments</p>
-        <p>add comments</p>
+      <div className='content-post'>
+        <div className='icons-wrapper'>
+          {/* <HeartIcon className='icon not-like' /> */}
+          <HeartIconSolid className='icon like' />
+          <ChatIcon className='icon' />
+        </div>
+        <div className='likes-wrapper'>
+          <p>2 likes</p>
+        </div>
+        <ul className='comments-wrapper'>
+          {comments.map(({ author, comments }, index) => {
+            return (
+              <li key={index}>
+                <span className='author'>{author}</span>{' '}
+                <span className='comment'>{comments}</span>
+              </li>
+            );
+          })}
+        </ul>
+        <div className='date-wrapper'>
+          <p>5 month ago</p>
+        </div>
+      </div>
+      <div className='add-comment-wrapper'>
+        <input type='text' name='' id='' placeholder='Add a comment...' />
+        <button>Post</button>
       </div>
     </Wrapper>
   );
