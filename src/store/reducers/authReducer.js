00accc8d89c289f85d8b2865f317ee475655449c
Celdama <celdama.dev@gmail.com>
2022-03-16 19:02:38 +0100

    store/reducers/authReducer: UPDATE_USER

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,10 +1,13 @@
 export const REGISTER_USER = 'REGISTER_USER';
+export const UPDATE_USER = 'UPDATE_USER';
 
 const initialState = {};
 
 export const authReducer = (state = initialState, action) => {
   switch (action.type) {
     case REGISTER_USER:
+      return { ...state, ...action.payload };
+    case UPDATE_USER:
       return { ...state, ...action.payload };
     default:
       return state;
