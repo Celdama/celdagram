60068a0f62403c4c63bf41a990394b5c47a9a88e
Celdama <celdama.dev@gmail.com>
2022-03-16 19:00:15 +0100

    store/index: add authReducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { postsReducer } from './reducers/postsReducer';
+import { authReducer } from './reducers/authReducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 const store = createStore(
   combineReducers({
     posts: postsReducer,
+    auth: authReducer,
   }),
   composeWithDevTools(applyMiddleware(thunk))
 );
