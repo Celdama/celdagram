0cfd23f9528c32e8645b94129f0525daff8a2892
Celdama <celdama.dev@gmail.com>
2022-03-16 19:04:32 +0100

    store/actions/authAction: updateUser

--- src/store/actions/authAction.js
+++ src/store/actions/authAction.js
@@ -1,5 +1,9 @@
-import { REGISTER_USER } from '../reducers/authReducer';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { REGISTER_USER, UPDATE_USER } from '../reducers/authReducer';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from 'firebase/auth';
 
 import { auth } from '../../config/firebaseConfig';
 
@@ -14,6 +18,20 @@ export const registerUser = (registerEmail, registerPassword) => {
       dispatch({
         type: REGISTER_USER,
         payload: userCredential.user,
+      });
+    } catch (err) {
+      return console.log(err);
+    }
+  };
+};
+
+export const updateUser = (username, avatar) => {
+  const auth = getAuth();
+  return async (dispatch) => {
+    try {
+      await updateProfile(auth.currentUser, {
+        displayName: username,
+        photoURL: avatar,
       });
     } catch (err) {
       return console.log(err);
