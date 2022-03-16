87bcab1d38acdd7506310284e53427a05e360221
Celdama <celdama.dev@gmail.com>
2022-03-16 19:09:44 +0100

    add monitorAuthState action to prevent auto logout on refresh page

--- src/store/actions/authAction.js
+++ src/store/actions/authAction.js
@@ -1,8 +1,13 @@
-import { REGISTER_USER, UPDATE_USER } from '../reducers/authReducer';
+import {
+  REGISTER_USER,
+  UPDATE_USER,
+  MONITOR_AUTH_STATE,
+} from '../reducers/authReducer';
 import {
   getAuth,
   createUserWithEmailAndPassword,
   updateProfile,
+  onAuthStateChanged,
 } from 'firebase/auth';
 
 import { auth } from '../../config/firebaseConfig';
@@ -32,6 +37,35 @@ export const updateUser = (username, avatar) => {
       await updateProfile(auth.currentUser, {
         displayName: username,
         photoURL: avatar,
+      });
+      dispatch({
+        type: UPDATE_USER,
+        payload: null,
+      });
+    } catch (err) {
+      return console.log(err);
+    }
+  };
+};
+
+export const monitorAuthState = () => {
+  return async (dispatch) => {
+    try {
+      onAuthStateChanged(auth, (user) => {
+        let currentUser;
+        if (user) {
+          const { email, uid, displayName, photoURL } = user.auth.currentUser;
+          currentUser = {
+            email,
+            uid,
+            displayName,
+            photoURL,
+          };
+        }
+        dispatch({
+          type: MONITOR_AUTH_STATE,
+          payload: currentUser,
+        });
       });
     } catch (err) {
       return console.log(err);
--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,5 +1,6 @@
 export const REGISTER_USER = 'REGISTER_USER';
 export const UPDATE_USER = 'UPDATE_USER';
+export const MONITOR_AUTH_STATE = 'MONITOR_AUTH_STATE';
 
 const initialState = {};
 
@@ -8,6 +9,8 @@ export const authReducer = (state = initialState, action) => {
     case REGISTER_USER:
       return { ...state, ...action.payload };
     case UPDATE_USER:
+      return { ...state };
+    case MONITOR_AUTH_STATE:
       return { ...state, ...action.payload };
     default:
       return state;
