a3c41a097ae7a17bfd02d6d6b191952f86b0ef04
Celdama <celdama.dev@gmail.com>
2022-03-17 17:06:22 +0100

    components/Profile/index: add logic to display user photo in Profile component

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { postsSelector } from '../../store/selectors/postsSelector';
 import { usersSelector } from '../../store/selectors/usersSelector';
 import { Wrapper, Photo, UserInfo, UserPhotos } from './profile.style';
 
-export const Profile = ({ users, id }) => {
+export const Profile = ({ users, id, posts }) => {
   const user = users.filter((user) => user.uid === id)[0];
+
+  const userPosts = posts.filter((post) => post.userId === user.uid);
 
   return (
     <Wrapper>
@@ -17,18 +20,18 @@ export const Profile = ({ users, id }) => {
             <button>Follow</button>
           </div>
           <div className='social'>
-            <span>5 photos</span>
+            <span>
+              {userPosts.length} photo{userPosts.length > 1 ? 's' : ''}
+            </span>
             <span>1 followers</span>
             <span>0 following</span>
           </div>
         </div>
       </UserInfo>
       <UserPhotos>
-        <Photo imgUrl='https://images.unsplash.com/photo-1522439773404-99299bcdad5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80' />
-        <Photo imgUrl='https://images.unsplash.com/photo-1647185255710-bc523f457ffd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80' />
-        <Photo imgUrl='https://images.unsplash.com/photo-1647249935230-11b4bb6dd95e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80' />
-        <Photo imgUrl='https://images.unsplash.com/photo-1647264002131-7f00ef7a2bf4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=682&q=80' />
-        <Photo imgUrl='https://images.unsplash.com/photo-1647189892164-40f05ebcbefa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=723&q=80' />
+        {userPosts.map((post, index) => (
+          <Photo key={index} imgUrl={post.photoURL} />
+        ))}
       </UserPhotos>
     </Wrapper>
   );
@@ -37,6 +40,7 @@ export const Profile = ({ users, id }) => {
 export const ProfileStore = () => {
   const { id } = useParams();
   const users = useSelector(usersSelector);
+  const posts = useSelector(postsSelector);
 
-  return <Profile users={users} id={id} />;
+  return <Profile users={users} id={id} posts={posts} />;
 };
