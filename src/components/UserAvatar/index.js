03a15590deb2f657088d0599336059c03d7f55fa
Celdama <celdama.dev@gmail.com>
2022-03-17 09:52:14 +0100

    components/UserAvatar/index: remove useless console.log(

--- src/components/UserAvatar/index.js
+++ src/components/UserAvatar/index.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import { Wrapper, Avatar, Name } from './userAvatar.style';
 
 export const UserAvatar = ({ id, url, name, size = 50 }) => {
-  console.log(id);
   return (
     <Wrapper>
       <Link to={`/profile/${id && id}`}>
