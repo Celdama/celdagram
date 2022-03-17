614133bc18046f1cbba700077e10b2982f5a15e9
Celdama <celdama.dev@gmail.com>
2022-03-17 09:33:50 +0100

    layouts/NavBar/index: display authUser avatar in navbar

--- src/layouts/NavBar/index.js
+++ src/layouts/NavBar/index.js
@@ -10,7 +10,6 @@ import {
 import { logOutUser } from '../../store/actions/authAction';
 
 export const NavBar = ({ isAuth, authUser, handleLogOut }) => {
-  console.log(authUser);
   return (
     <Wrapper>
       <Content className='content'>
@@ -25,10 +24,7 @@ export const NavBar = ({ isAuth, authUser, handleLogOut }) => {
                 <LogoutIcon className='icon' />
               </li>
               <Link to={`/profile/${authUser.uid}`}>
-                <img
-                  src='https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'
-                  alt=''
-                />
+                <img src={authUser && authUser.photoURL} alt='' />
               </Link>
             </>
           ) : (
