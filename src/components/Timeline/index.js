b71562e82bf0e20d9b99c1acd9c911a2ef372631
Celdama <celdama.dev@gmail.com>
2022-03-18 09:15:39 +0100

    components/Timeline/index: add logic to display only post of user follow

--- src/components/Timeline/index.js
+++ src/components/Timeline/index.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { authSelector } from '../../store/selectors/authSelector';
 import { postsSelector } from '../../store/selectors/postsSelector';
+import { usersSelector } from '../../store/selectors/usersSelector';
 
 import { PostStore } from '../Post';
 import { Wrapper, Content } from './timeline.style';
 
-export const Timeline = ({ posts }) => {
+export const Timeline = ({ posts, authUser, currentUser }) => {
+  // console.log(currentUser.followings);
+  // console.log(posts.userId);
+
+  const followings = currentUser && currentUser.followings;
+
+  const postsContent = posts.filter((post) =>
+    followings.some((e) => e.uid === post.userId)
+  );
+
+  console.log(!postsContent);
+
   return (
     <Wrapper>
       <Content>
-        {posts &&
-          posts.map((post, index) => <PostStore key={index} post={post} />)}
+        {/* {posts &&
+          postsContent.map((post, index) => (
+            <PostStore key={index} post={post} />
+          ))} */}
+        {!postsContent ? (
+          postsContent.map((post, index) => (
+            <PostStore key={index} post={post} />
+          ))
+        ) : (
+          <p>you need to follow</p>
+        )}
       </Content>
     </Wrapper>
   );
@@ -18,6 +40,12 @@ export const Timeline = ({ posts }) => {
 
 export const TimelineStore = () => {
   const posts = useSelector(postsSelector);
+  const authUser = useSelector(authSelector);
+  const users = useSelector(usersSelector);
 
-  return <Timeline posts={posts} />;
+  const currentUser = users.filter((user) => user.uid === authUser.uid)[0];
+
+  return (
+    <Timeline posts={posts} authUser={authUser} currentUser={currentUser} />
+  );
 };
