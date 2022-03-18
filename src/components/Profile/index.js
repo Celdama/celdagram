978f827d904e10bfa7d45e2f817c8ced1c5ade12
Celdama <celdama.dev@gmail.com>
2022-03-18 07:46:14 +0100

    components/Profile/index: add onClick action to unfollow user

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,21 +1,37 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { removeFollowing } from '../../store/actions/usersAction';
 import { authSelector } from '../../store/selectors/authSelector';
 import { postsSelector } from '../../store/selectors/postsSelector';
 import { usersSelector } from '../../store/selectors/usersSelector';
 import { Wrapper, Photo, UserInfo, UserPhotos } from './profile.style';
 
 export const Profile = ({ users, id, posts, currentUser }) => {
+  const dispatch = useDispatch();
   const user = users.filter((user) => user.uid === id)[0];
 
   const userPosts = posts.filter((post) => post.userId === user.uid);
+
+  const handleRemoveFollowing = (currentUserId, userToUnfollow) => {
+    dispatch(removeFollowing(currentUserId, userToUnfollow));
+  };
 
   const socialBtn =
     currentUser && !currentUser.followings.some((e) => e.uid === id) ? (
       <button>follow</button>
     ) : (
-      <button>unfollow</button>
+      <button
+        onClick={() =>
+          handleRemoveFollowing(currentUser.uid, {
+            avatar: user.avatar,
+            uid: user.uid,
+            username: user.username,
+          })
+        }
+      >
+        unfollow
+      </button>
     );
 
   return (
