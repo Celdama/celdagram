07aa2a4d17e83fd7f60a40d4f4ecfa79ae7e8071
Celdama <celdama.dev@gmail.com>
2022-03-18 07:18:20 +0100

    components/Suggestion/index: add conditional statement to not rerender a suggestion already follow

--- src/components/Suggestion/index.js
+++ src/components/Suggestion/index.js
@@ -1,43 +1,57 @@
 import React from 'react';
 import { UserAvatar } from '../UserAvatar';
 import { addFollowing } from '../../store/actions/usersAction';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { usersSelector } from '../../store/selectors/usersSelector';
 
-export const SuggestionList = ({ authUser, suggestions }) => {
+export const SuggestionList = ({ authUser, suggestions, currentUser }) => {
   const dispatch = useDispatch();
 
   const handleAddFollowing = (authUserId, userId) => {
     dispatch(addFollowing(authUserId, userId));
   };
 
+  const suggestionsContent = suggestions.map(({ uid, avatar, username }) => {
+    if (!currentUser.followings.some((e) => e.uid === uid)) {
+      return (
+        <div key={uid}>
+          <UserAvatar id={uid} url={avatar} name={username} size={30} />
+          <button
+            onClick={() =>
+              handleAddFollowing(authUser.uid, {
+                avatar,
+                uid,
+                username,
+              })
+            }
+          >
+            Follow
+          </button>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  });
+
   return (
     <div className='content-user-suggestion'>
       <h5>Suggestions for you</h5>
-      <div className='suggestion-user'>
-        {suggestions &&
-          suggestions.map(({ uid, avatar, username }) => {
-            return (
-              <div key={uid}>
-                <UserAvatar id={uid} url={avatar} name={username} size={30} />
-                <button
-                  onClick={() =>
-                    handleAddFollowing(authUser.uid, {
-                      avatar,
-                      uid,
-                      username,
-                    })
-                  }
-                >
-                  Follow
-                </button>
-              </div>
-            );
-          })}
-      </div>
+      <div className='suggestion-user'>{suggestions && suggestionsContent}</div>
     </div>
   );
 };
 
 export const SuggestionListStore = ({ authUser, suggestions }) => {
-  return <SuggestionList suggestions={suggestions} authUser={authUser} />;
+  const currentUser = useSelector(usersSelector).filter(
+    (user) => user.uid === authUser.uid
+  )[0];
+
+  return (
+    <SuggestionList
+      suggestions={suggestions}
+      authUser={authUser}
+      currentUser={currentUser}
+    />
+  );
 };
