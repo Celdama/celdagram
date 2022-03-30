c7f10c61253caa0a7843c5beb996553c361b361f
Celdama <celdama.dev@gmail.com>
2022-03-30 17:21:38 +0200

    config/firebaseConfig: add storage config

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyB83rScDOC19T_BLxizjQYFm24TzE1Dl6s',
@@ -15,3 +16,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
 export const auth = getAuth(app);
+export const storage = getStorage(app);
