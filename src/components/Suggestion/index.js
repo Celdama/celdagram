d52eea7ba786e8acbc6072bd12be262f140038d2
Celdama <celdama.dev@gmail.com>
2022-03-31 19:00:13 +0200

    components/Suggestion/index: refactoring

--- src/components/Suggestion/index.js
+++ src/components/Suggestion/index.js
@@ -18,8 +18,8 @@ export const SuggestionList = ({ authUser, suggestions, currentUser }) => {
     );
   };
 
-  const suggestionsContent = suggestions.map(({ uid, avatar, username }) => {
-    if (!currentUser.followings.some((e) => e.uid === uid)) {
+  const suggestionsList = suggestions.map(({ uid, avatar, username }) => {
+    if (!currentUser.followings.some((follow) => follow.uid === uid)) {
       return (
         <div key={uid}>
           <UserAvatar id={uid} url={avatar} name={username} size={30} />
@@ -44,7 +44,7 @@ export const SuggestionList = ({ authUser, suggestions, currentUser }) => {
   return (
     <div className='content-user-suggestion'>
       <h5>Suggestions for you</h5>
-      <div className='suggestion-user'>{suggestions && suggestionsContent}</div>
+      <div className='suggestion-user'>{suggestions && suggestionsList}</div>
     </div>
   );
 };
