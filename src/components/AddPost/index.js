c7bb600e24d8b7f955e361847be74c228d7e2d21
Celdama <celdama.dev@gmail.com>
2022-03-31 09:01:19 +0200

    components/AddPost/index: add content

--- src/components/AddPost/index.js
+++ src/components/AddPost/index.js
@@ -1,5 +1,28 @@
 import React from 'react';
 
 export const index = () => {
-  return <div>index</div>;
+  const handleChange = () => {};
+
+  const handlePhotoChange = () => {};
+
+  const handleAddPost = () => {};
+  return (
+    <div>
+      <form>
+        <div>
+          <label htmlFor='description'>Description</label>
+          <input type='text' name='description' onChange={handleChange} />
+        </div>
+        <div>
+          <label htmlFor='photo'>Photo</label>
+          <input type='file' name='photo' onChange={handlePhotoChange} />
+        </div>
+        <div>
+          <button type='button' onClick={handleAddPost}>
+            Add Post
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
