e38cfeccd562d37ac438c5cdceda4e6498d82430
Celdama <celdama.dev@gmail.com>
2022-04-05 18:13:15 +0200

    components/Post/ReactPortal/index: update ReactPortal

--- src/components/Post/ReactPortal/index.js
+++ src/components/Post/ReactPortal/index.js
@@ -1,7 +1,14 @@
 import { createPortal } from 'react-dom';
+import { createWrapperAndAppendToBody } from '../../../Helpers/createWrapperAndAppendToBody';
 
-const ReactPortal = ({ children, wrapperId }) => {
-  return createPortal(children, document.getElementById(wrapperId));
+const ReactPortal = ({ children, wrapperId = 'react-portal-wrapper' }) => {
+  let element = document.getElementById(wrapperId);
+
+  if (!element) {
+    element = createWrapperAndAppendToBody(wrapperId);
+  }
+
+  return createPortal(children, element);
 };
 
 export default ReactPortal;
