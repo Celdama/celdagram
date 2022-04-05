9c6957ff6f197b187268ef149edd01d5b409e2c0
Celdama <celdama.dev@gmail.com>
2022-04-05 17:37:56 +0200

    components/Profile/index: add function to deletePost in Profile component

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,13 +1,19 @@
-import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useCallback, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { postsSelector } from '../../store/selectors/postsSelector';
 import { usersSelector } from '../../store/selectors/usersSelector';
 import { Wrapper, Photo, UserPhotos, PostWrapper } from './profile.style';
 import { ProfileUserInfo } from './ProfileUserInfo';
 import { HeartIcon, ChatIcon } from '@heroicons/react/solid';
+import { deletePost } from '../../store/actions/postsAction';
 
-export const Profil = ({ userProfile, currentProfileId, posts }) => {
+export const Profil = ({
+  userProfile,
+  currentProfileId,
+  posts,
+  deletePostFromFirebase,
+}) => {
   const [isLoaded, setIsLoaded] = useState(false);
 
   const userPosts = posts.filter((post) => post.userId === userProfile.uid);
@@ -20,6 +26,7 @@ export const Profil = ({ userProfile, currentProfileId, posts }) => {
 
   const handleDeletePost = (id) => {
     console.log('delete this post', id);
+    deletePostFromFirebase(id);
   };
 
   return (
@@ -61,10 +68,23 @@ export const ProfileStore = () => {
   const { id } = useParams();
   const users = useSelector(usersSelector);
   const posts = useSelector(postsSelector);
+  const dispatch = useDispatch();
 
   const userProfile = users.filter((user) => user.uid === id)[0];
 
+  const deletePostFromFirebase = useCallback(
+    (postId) => {
+      dispatch(deletePost(postId));
+    },
+    [dispatch]
+  );
+
   return (
-    <Profil userProfile={userProfile} currentProfileId={id} posts={posts} />
+    <Profil
+      userProfile={userProfile}
+      currentProfileId={id}
+      posts={posts}
+      deletePostFromFirebase={deletePostFromFirebase}
+    />
   );
 };
