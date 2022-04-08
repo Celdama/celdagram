5b5c9bc6d0e880c4cdad2a4c598bd0eeb9adb891
Celdama <celdama.dev@gmail.com>
2022-04-08 11:35:34 +0200

    auth/SignUp/index:

--- src/auth/SignUp/index.js
+++ src/auth/SignUp/index.js
@@ -42,7 +42,6 @@ export const SignUp = ({ registerUserInFirebase }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     const avatarRef = ref(storage, `${username}-avatar`);
 
     try {
