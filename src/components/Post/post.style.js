3b83132b858a0f33ed7b58c46acaf9675fdc6a59
Celdama <celdama.dev@gmail.com>
2022-04-15 10:23:22 +0200

    components/Post/post.style: add css variable

--- src/components/Post/post.style.js
+++ src/components/Post/post.style.js
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.article`
   width: 504px;
-  margin-bottom: 30px;
-  outline: 1px solid lightgray;
-  background-color: #fff;
+  margin-bottom: 3rem;
+  outline: 1px solid var(--borderGray);
+  background-color: var(--white);
   border-radius: 4px;
 `;
 
@@ -22,7 +22,8 @@ export const ContentPost = styled.div`
 
   .show-all-comments {
     font-size: 1.2rem;
-    color: #6c757d;
+    outline: 1px solid red;
+    color: var(--darkGray);
     font-weight: bold;
     cursor: pointer;
   }
