5fa8e09bec8d3eb1bac006227de7c9712b673876
Celdama <celdama.dev@gmail.com>
2022-04-15 10:07:56 +0200

    components/Post/PostLikesCounter/postLikesCounter.style: update style

--- src/components/Post/PostLikesCounter/postLikesCounter.style.js
+++ src/components/Post/PostLikesCounter/postLikesCounter.style.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  margin: 0.4rem 0;
+  margin: 0;
   padding: 0;
-  font-size: 0.9rem;
+  font-size: 1.2rem;
   font-weight: bold;
 `;
